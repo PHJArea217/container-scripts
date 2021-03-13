e6cb537ec4af3fe09145f3d5183ad0c90ebb35be
Peter H. Jin <peter@peterjin.org>
2021-03-13 04:08:25 -0600

    Add generic syscall implementation

---
+++ ctrtool/arch/generic/arch-functions.h
@@ -0,0 +1,8 @@
+#include <syscall.h>
+long ctrtool_arch_generic_syscall(long nr, long a, long b, long c, long d, long e, long f);
+__attribute__((noreturn)) void ctrtool_arch_generic_exit(int status);
+#define CTRTOOL_SYS_pidfd_open SYS_pidfd_open
+#define CTRTOOL_SYS_pidfd_getfd SYS_pidfd_getfd
+#define ctrtool_syscall(nr, a, b, c, d, e, f) ctrtool_arch_generic_syscall(nr, (long) a, (long) b, (long) c, (long) d, (long) e, (long) f)
+#define ctrtool_clone_onearg(arg) ctrtool_syscall(SYS_clone, arg, 0, 0, 0, 0, 0)
+#define ctrtool_exit(arg) ctrtool_arch_generic_exit(arg)
---
+++ ctrtool/arch/generic/syscall.c
@@ -0,0 +1,18 @@
+#define _GNU_SOURCE
+#include <unistd.h>
+#include <syscall.h>
+#include <errno.h>
+long ctrtool_arch_generic_syscall(long nr, long a, long b, long c, long d, long e, long f) {
+	int saved_errno = errno;
+	errno = 0;
+	long ret_val = syscall(nr, a, b, c, d, e, f);
+	if (ret_val == -1) {
+		ret_val = -errno;
+	}
+	errno = saved_errno;
+	return ret_val;
+}
+__attribute__((noreturn)) void ctrtool_arch_generic_exit(int status) {
+	syscall(SYS_exit, status, 0, 0, 0, 0, 0);
+	while (1) ;
+}
