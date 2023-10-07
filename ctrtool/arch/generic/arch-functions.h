b0dbf7782cd2a81a685dbc0d019cb56c7fe67bff
Peter H. Jin <peter@peterjin.org>
2023-10-07 17:02:28 -0400

    arch/generic: Use clone3 for ctrtool_clone_onearg

--- ctrtool/arch/generic/arch-functions.h
+++ ctrtool/arch/generic/arch-functions.h
@@ -8,5 +8,5 @@ __attribute__((noreturn)) void ctrtool_arch_generic_exit(int status);
 #define CTRTOOL_SYS_openat2 __NR_openat2
 #define ctrtool_syscall(nr, a, b, c, d, e, f) ctrtool_arch_generic_syscall(nr, (long) (a), (long) (b), (long) (c), (long) (d), (long) (e), (long) (f))
 #define ctrtool_raw_syscall(nr, a, b, c, d, e, f) ctrtool_arch_generic_syscall(nr, (long) (a), (long) (b), (long) (c), (long) (d), (long) (e), (long) (f))
-#define ctrtool_clone_onearg(arg) ctrtool_syscall(SYS_clone, arg, 0, 0, 0, 0, 0)
+long ctrtool_clone_onearg(unsigned long arg);
 #define ctrtool_exit(arg) ctrtool_arch_generic_exit(arg)
--- ctrtool/arch/generic/syscall.c
+++ ctrtool/arch/generic/syscall.c
@@ -1,6 +1,7 @@
 #define _GNU_SOURCE
 #include <unistd.h>
 #include <syscall.h>
+#include <stdint.h>
 #include <errno.h>
 long ctrtool_arch_generic_syscall(long nr, long a, long b, long c, long d, long e, long f) {
 	int saved_errno = errno;
@@ -11,6 +12,12 @@ long ctrtool_arch_generic_syscall(long nr, long a, long b, long c, long d, long 
 	}
 	errno = saved_errno;
 	return ret_val;
+}
+long ctrtool_clone_onearg(unsigned long arg) {
+	uint64_t clone_args[8] = {0};
+	clone_args[0] = arg & -256L; /* flags */
+	clone_args[4] = arg & 255; /* exit_signal */
+	return ctrtool_arch_generic_syscall(__NR_clone3, (long) clone_args, sizeof(clone_args), 0, 0, 0, 0);
 }
 __attribute__((noreturn)) void ctrtool_arch_generic_exit(int status) {
 	syscall(SYS_exit_group, status, 0, 0, 0, 0, 0);
